798a6ea8bf8e4ffb12f88cdb2841d759be117855
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-13 15:22:05 +0200

    file:img_vsposts

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 3000;
 const url = "http://localhost:";
 
+// cartella public disponibile
+app.use(express.static("public"));
+
 // ascolto
 app.listen(port, () => {
   console.log(`server in ascolto su ${url}${port}`);
