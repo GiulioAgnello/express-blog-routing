ad94fd96ec9f9fcf902b97e9f496d45c43fa4229
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-13 17:33:44 +0200

    end_routers

--- routers/posts.js
+++ routers/posts.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const router = express.Router();
+let { posts } = require("../db");
 
 // index
 router.get(`/`, (req, res) => {
-  res.json(`lista dei post`);
+  res.json({
+    description: `lista dei post`,
+    data: posts,
+  });
 });
 
 // show
 router.get("/:id", (req, res) => {
-  const post = req.params.id;
-  res.json(`post n ${post}`);
-});
+  const id = parseInt(req.params.id);
+  const post = posts.find((currentPost) => currentPost.id === id);
 
+  res.json({
+    description: `post selezionato`,
+    data: post,
+  });
+});
 // store
 router.post(`/`, (req, res) => {
   res.json(`crea nuovo post`);
@@ -19,20 +27,24 @@ router.post(`/`, (req, res) => {
 
 // update
 router.put("/:id", (req, res) => {
-  const post = req.params.id;
+  const id = req.params.id;
   res.json(`modifica completa post n ${post}`);
 });
 
 // modify
 router.patch("/:id", (req, res) => {
-  const post = req.params.id;
+  const id = req.params.id;
   res.json(`modifica parziale post n ${post}`);
 });
 
 // destroy
 router.delete("/:id", (req, res) => {
-  const post = req.params.id;
-  res.json(`eliminazione del post n ${post}`);
+  const id = parseInt(req.params.id);
+  posts = posts.filter((currentPost) => currentPost.id !== id);
+  res.json({
+    description: `eliminato post n ${id}`,
+    data: posts,
+  });
 });
 
 // export
