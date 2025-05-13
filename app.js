94fdbfc49c8826aaa370ad00ac78978a395daf04
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-13 15:35:39 +0200

    add_routers

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// install express e port
+// import
+const { posts } = require("./db");
 
+// install express e port
 const express = require("express");
 const app = express();
 const port = 3000;
-const url = "http://localhost:";
+const url = `http://localhost:${port}/`;
+
+app.use("/");
 
 // cartella public disponibile
 app.use(express.static("public"));
 
 // ascolto
 app.listen(port, () => {
-  console.log(`server in ascolto su ${url}${port}`);
+  console.log(`server in ascolto su ${url}`);
 });
